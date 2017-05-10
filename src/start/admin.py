be99b1be3eead5465528a8b2c35835dd27834954
bryantest00 <apocalips_war@yahoo.com>
2017-05-10 15:10:27 -0700

    Columna Clasificacion

--- src/start/admin.py
+++ src/start/admin.py
@@ -5,12 +5,16 @@ from .models import Mensaje,Equipo, Clasificacion
 from .forms import RegContactForm, RegEquipoForm, RegClasificacionForm
 
 class AdminEquipo(admin.ModelAdmin):
-    list_display = ['clasificacion', 'nombre', 'modelo', 'marca', 'timestamp']
+    list_display = ['clasif_clasif','nombre', 'modelo', 'marca', 'timestamp']
     form = RegEquipoForm
     list_display_links = ['nombre']
     list_filter = ['timestamp']
     list_editable = ['modelo']
-    search_fields = ['clasificacion', 'nombre', 'modelo', 'marca']
+    search_fields = ['nombre', 'modelo', 'marca']
+
+    def clasif_clasif(self,obj):
+        return obj.clasificacion.clasificacion
+    clasif_clasif.short_description = 'Clasificacion'
 
 class AdminContacto(admin.ModelAdmin):
     list_display = ['email', 'nombre', 'timestamp']
