ea3d3358457c3ff2580ce7cb2949f9368bc5ef2a
bryantest00 <apocalips_war@yahoo.com>
2017-05-26 08:45:11 -0700

    SlideShow

--- src/start/forms.py
+++ src/start/forms.py
@@ -2,20 +2,11 @@ __author__ = 'NeroS'
 # -*- coding: utf-8 -*-
 
 from django import forms
-<<<<<<< HEAD
 from .models import Mensaje, Equipo, Categoria, Slideshow
-=======
-# from crispy_forms.bootstrap import AppendedText, PrependedText
-# from crispy_forms.helper import FormHelper
-# from crispy_forms.layout import Layout, ButtonHolder, Submit
-# from crispy_forms.layout import Field
-from .models import Mensaje, Equipo, Categoria
->>>>>>> a2c84bb6414900b317fa31774f5e74fdb247d49f
 
 class RegEquipoForm(forms.ModelForm):
     class Meta:
         model = Equipo
-<<<<<<< HEAD
         fields = ['categoria', 'nombre', 'modelo', 'marca', 'so', 'procesador',
                   'harddrive', 'pantalla', 'ram', 'video', 'precio',
                   'descripcion', 'imagen']
@@ -29,25 +20,11 @@ class RegCategoriaForm(forms.ModelForm):
     class Meta:
         model = Categoria
         fields = ['categoria']
-=======
-        fields = ['categoria','nombre', 'modelo', 'marca','so', 'procesador',
-                  'harddrive', 'pantalla', 'ram', 'video',
-                  'precio', 'imagen']
-
-class RegCategoriaForm(forms.ModelForm):
-	class Meta:
-		model = Categoria
-		fields = ['categoria']
->>>>>>> a2c84bb6414900b317fa31774f5e74fdb247d49f
 
 class RegContactForm(forms.ModelForm):
     class Meta:
         model = Mensaje
-<<<<<<< HEAD
         fields = ['nombre', 'email', 'telefono', 'asunto', 'mensaje']
-=======
-        fields = ['nombre', 'email', 'telefono', 'tema', 'mensaje']
->>>>>>> a2c84bb6414900b317fa31774f5e74fdb247d49f
     def __init__(self, *args, **kwargs):
         super(RegContactForm, self).__init__(*args, **kwargs)
         for field_name in self.fields:
--- src/start/templatetags/filtro_tag.py
+++ src/start/templatetags/filtro_tag.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django import template
 from start.models import Categoria
 
@@ -7,13 +6,3 @@ register = template.Library()
 @register.assignment_tag
 def categorias_lista():
 	return Categoria.objects.all()
-=======
-from django import template
-from start.models import Categoria
-
-register = template.Library()
-
-@register.assignment_tag
-def categorias_lista():
-	return Categoria.objects.all()
->>>>>>> a2c84bb6414900b317fa31774f5e74fdb247d49f
