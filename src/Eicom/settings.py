f5dc8327392c80cd93f022644eeada1eb5ce149f
Eicom <eicomtecnologias@gmail.com>
2017-07-26 20:12:02 +0000

    Fix template

--- src/Eicom/settings.py
+++ src/Eicom/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 EMAIL_HOST = 'smtp.gmail.com'
